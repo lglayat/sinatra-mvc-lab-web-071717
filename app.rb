2c0adddc6b9856321adb50089be9b01d8fce5b1c
lglayat <glayatle@g.cofc.edu>
2017-08-09 10:40:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,28 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post "/piglatinize" do
+    @val = params[:user_phrase]
+    # PigLatinizer.new(@val)
+
+    @word = PigLatinizer.new
+    # binding.pry
+
+    erb :result
+  end
+
+
+
+
+
+
+
+
+
+
 end
